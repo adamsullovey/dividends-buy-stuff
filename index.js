fbf06316a4545921f8a9bdfef459ed6b216e8c15
Adam Sullovey <adam.sullovey@gmail.com>
2014-12-17 11:48:04 -0500

    wrote a bunch of functions to answer the question I'm asking. It works! Time to refine

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var Q = require('q'),
-	request = require('request');
-
+	R = require('ramda'),
+	request = require('request'),
+	util = require('util');
 
 var stocks = [{
 	ticker: 'WB.TO',
 	goodsPrice: 2000,
-	goodsName: "Season's pass"
+	goodsName: "Whistler Blackcomb Season's pass"
+}, {
+	ticker: 'AAPL',
+	goodsPrice: 649.00,
+	goodsName: 'unlocked iPhone 6'
 }];
 
 var getStockDividendHistory = function (data) {
@@ -23,7 +28,7 @@ var getStockDividendHistory = function (data) {
 		if (error) {
 			deferred.reject(error);
 		} else {
-			data.dividendhistory = body.query.results.quote;
+			data.dividendHistory = body.query.results.quote;
 			deferred.resolve(data);
 		}
 
@@ -33,23 +38,64 @@ var getStockDividendHistory = function (data) {
 
 };
 
+var getYearOfDividends = function (data) {
 
-var getStockValueAtDate = function (data) {
+	// assume dividends issued quarterly
+	// 4 dividends in 1 year
+
+	function stringToNumber(value) {
+		return value * 1;
+	}
+
+
+
+	/*
+	 * read code from the bottom up, or this comment top down:
+	 * take first 4 elements of array
+	 * pluck the 'Dividends' property (ditch all other info)
+	 * convert the strings like '0.2444' to numbers
+	 * add up all the numbers
+	 *
+	 */
+
+	var dividendValue = R.sum(
+		R.map(stringToNumber,
+			R.pluck('Dividends')(
+				R.slice(0, 4)(data.dividendHistory))));
+
+
+	var dividendInfo = {
+		value: dividendValue,
+		startDate: new Date(data.dividendHistory[3].Date),
+		endDate: new Date(data.dividendHistory[0].Date),
+		startDateString: data.dividendHistory[3].Date,
+		endDateString: data.dividendHistory[0].Date
+	};
+
+	// TODO - buy date should be a day before startDate. You need to own the stock to get the dividend
+
+	data.yearlyDividendInfo = dividendInfo;
+
+	return data;
+
+
+};
+
+var getStockInfoAtDate = function (data) {
 
 	var options = {
-		url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22' + data.ticker + '%22%20and%20startDate%20%3D%20%222012-09-11%22%20and%20endDate%20%3D%20%222013-03-10%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
+		url: util.format('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22%s%22%20and%20startDate%20%3D%20%22%s%22%20and%20endDate%20%3D%20%22%s%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', data.ticker, data.yearlyDividendInfo.startDateString, data.yearlyDividendInfo.startDateString),
 		json: true
 	};
 
 	var deferred = Q.defer();
 
 	request(options, function (error, response, body) {
-		//console.log(body.query.results);
 
 		if (error) {
 			deferred.reject(error);
 		} else {
-			data.priceAtDate = body.query.results.quote[0];
+			data.stockInfoAtDate = body.query.results.quote;
 			deferred.resolve(data);
 		}
 
@@ -59,13 +105,49 @@ var getStockValueAtDate = function (data) {
 
 };
 
+var calculateShareOwnership = function (data) {
+
+	var sharesNeeded = Math.ceil(data.goodsPrice / data.yearlyDividendInfo.value);
+
+	var ownershipInfo = {
+		sharesNeeded: sharesNeeded,
+		purchaseCostOfShares: sharesNeeded * data.stockInfoAtDate.Low
+	};
+
+	data.ownershipInfo = ownershipInfo;
+
+	return data;
+
+};
+
+var niceOutput = function (data) {
+
+	var copy = "To buy %s at $%d, you could have bought %d shares of %s on %s for $%d";
+	copy = util.format(copy, data.goodsName, data.goodsPrice, data.ownershipInfo.sharesNeeded, data.ticker, data.yearlyDividendInfo.startDateString, data.ownershipInfo.purchaseCostOfShares);
+
+	data.niceOutput = copy;
+
+	return data;
+
+};
+
 var data = stocks[0];
 
+/*
 getStockDividendHistory(data)
-	.then(getStockValueAtDate)
+	.then(getStockInfoAtDate)
 	.then(function (value) {
 		console.log(value);
 	});
+*/
+
+var pipedFunctions = R.pPipe(getStockDividendHistory, getYearOfDividends, getStockInfoAtDate, calculateShareOwnership, niceOutput);
+
+pipedFunctions(data).then(function (value) {
+	console.log('done!', value);
+}).then(null, function (error) {
+	console.log('error woo', error);
+});
 
 /*
 1. Start with
